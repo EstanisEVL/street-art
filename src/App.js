fded8e1eabbba08fcc9ff4fff998e93ee1e20bf1
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2023-04-11 15:27:01 -0300

    Hero section fixes 2.0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 /*---- Imported components ----*/
 import Hero from "./components/hero/Hero";
 import NavBar from "./components/navbar/NavBar";
-// import GalleryFinal from "./components/gallery3/GalleryFinal";
-// import GalleryItemListAlt from "./components/gallery2/GalleryItemListAlt";
+import GalleryFinal from "./components/gallery3/GalleryFinal";
+import GalleryItemListAlt from "./components/gallery2/GalleryItemListAlt";
 /*---- Imported styles ----*/
 import "./styles/styles.scss";
 
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
+      <NavBar />
       <Hero />
       {/* <GalleryAlt slides={GalleryItemListAlt} /> */}
       {/* <GalleryFinal slides={GalleryItemListAlt} /> */}
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,21 +1,33 @@
 import HeroBtn from "../buttons/HeroBtn";
-import heroImg from "../../assets/imgs/uncompressed/hero-img-frida.jpg";
 import { ReactComponent as HeroLogo } from "../../assets/svg/hero-logo.svg";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
+  // LIMITAR EL USEEFFECT CON INTERSECTION OBSERVER ASIGNANDO UNA REFERENCIA AL CONTENEDOR PARA CUANDO ÉSTE NO ESTÉ EN PANTALLA DEJE DE ACTIVARSE EL USEEFFECT
+  const [value, setValue] = useState(1);
+
+  useEffect(() => {
+    const reduceOpacity = () => {
+      setValue(1 - `${window.scrollY / 1000}`);
+      console.log(value);
+    };
+
+    window.addEventListener("scroll", reduceOpacity);
+    return () => {
+      window.removeEventListener("scroll", reduceOpacity);
+    };
+  }, []);
+
   return (
-    <>
-      <div className="hero-container">
-        <div className="hero_img-container">
-          <img src={heroImg} />
-        </div>
+    <div className="hero_bg-container">
+      <div className="hero-container" style={{ opacity: value }}>
         <div className="hero_wrapper-txt">
           <div className="title">Street art project</div>
           <HeroLogo />
         </div>
+        <HeroBtn />
       </div>
-      <HeroBtn />
-    </>
+    </div>
   );
 };
 
